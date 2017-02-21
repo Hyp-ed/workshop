3fb9d2299312bdb9f6b63a22bd81454e8ee58770
Brano Pilnan <b.pilnan@gmail.com>
2017-02-21 20:48:32 +0000

    proximity stage 1 debugged

--- workshop/proximity-task1.py
+++ workshop/proximity-task1.py
@@ -1,38 +1,51 @@
 #!/usr/bin/python
 from vl6180 import Proximity
-import threading
+import thread
+import time
+
+def get_avg_dist_transaction(n, data_out, lock):
+    lock.acquire()
+    sum = 0
+    for i in xrange(0, int(n)):
+        sum += p.get_distance()
+    data_out[0] = float(sum) / n
+    lock.release()
 
 p = Proximity()
 
 def get_avg_dist(num_measurements):
-    sum = 0
-    for i in xrange(0, int(num_measurements)):
-        sum += p.get_distance()
-    return float(sum) / num_measurements
+    lock = thread.allocate_lock()
+    data = [0]
+    thread.start_new_thread(get_avg_dist_transaction, (num_measurements, data, lock))
+    time.sleep(0.001)
+    lock.acquire()
+    result = data[0]
+    lock.release()
+    return result
 
 # Initialize
 print 'Measuring distance to wall...'
-base_dist = get_avg_dist(32)
-print 'Distance to wall is {0:.3f} mm'.format(base_dist)
-n = 8
-tolerance = 2.5
+base_dist = get_avg_dist(50)
+print 'Distance to wall is {0:.0f} mm'.format(base_dist)
+n = 40
+tolerance = 3.5
 last_dist = base_dist
 
 while True:
     d = get_avg_dist(n)
-    if d > last_width + tolerance:
-        # New object
-        width = d - base_dist
-        print 'Object detected with width of {:.3f} mm'.format(width)
-        last_dist = d
     if d < last_dist - tolerance:
+        # New object
+        width = base_dist - d
+        print 'Object detected with width of {:.0f} mm'.format(width)
+        last_dist = d
+    if d > last_dist + tolerance:
         # New (narrower) object or no object
-        if d < base_dist + tolerance:
+        if d > base_dist - tolerance:
             # No object present
             print 'Object removed'
             last_dist = base_dist
         else:
             # New object
-            width = d - base_dist
-            print 'Object detected with width of {:.3f} mm'.format(width)
+            width = base_dist - d
+            print 'Object detected with width of {:.0f} mm'.format(width)
             last_dist = d
